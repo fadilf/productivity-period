5399b36cdbb4f9fbfa388bf2143b891f63cd3d3a
Fadil Eledath <fadilfaizal@gmail.com>
2019-09-21 19:49:36 -0500

    Functions file completed

--- Backend/functions.php
+++ Backend/functions.php
@@ -38,17 +38,60 @@ function sign_up($user_email, $password){
 }
 
 /*
- *	Verifies a username-password combination as existing within
- *	the Users table.
+ *	Fetches all of the data associated with a user based on user ID.
+ *	
+ *	Inputs:
+ *		- $user_ID: an integer containing the ID of the user
+ *	
+ *	Outputs: the user info array if user-password combo exists, -1 if
+ *	the ID is not in the table.
+ */
+function fetch_user_ID($user_ID){
+	global $mysqli;
+	$query = "SELECT `Email`, `Password`, `Session Active`, `Session ID`, `Latest Session Score` FROM Users WHERE ID=?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("i", $user_ID);
+	$stmt->execute();
+	$stmt->bind_result(
+		$user_email,
+		$pass_hash,
+		$session_active,
+		$session_ID,
+		$latest_session_score
+	);
+	$result = $stmt->fetch();
+	$stmt->close();
+	
+	// Checks if email exists in table
+	if($result){
+		
+		// Creates user info array to return
+		$user = [
+			"ID" => $user_ID,
+			"Email" => $user_email,
+			"Password Hash" => $pass_hash,
+			"Session Active" => $session_active,
+			"Session ID" => $session_ID,
+			"Current Session Score" => $latest_session_score
+		];
+
+		return $user;
+	} else {
+		// No user exists with given ID
+		return -1;
+	}
+}
+
+/*
+ *	Fetches all of the data associated with a user based on email address.
  *	
  *	Inputs:
  *		- $user_email: a string containing the email of the user
- *		- $password: a string containing the password of the user
  *	
- *	Outputs: the user info array if user-password combo exists, 0 if
- *	the password is incorrect and -1 if the email is not in the table.
+ *	Outputs: the user info array if user-password combo exists, -1 if
+ *	the email is not in the table.
  */
-function verify_user($user_email, $password){
+function fetch_user_email($user_email){
 	global $mysqli;
 	$query = "SELECT `ID`, `Password`, `Session Active`, `Session ID`, `Latest Session Score` FROM Users WHERE Email=?";
 	$stmt = $mysqli->prepare($query);
@@ -67,17 +110,47 @@ function verify_user($user_email, $password){
 	// Checks if email exists in table
 	if($result){
 		
+		// Creates user info array to return
+		$user = [
+			"ID" => $user_ID,
+			"Email" => $user_email,
+			"Password Hash" => $pass_hash,
+			"Session Active" => $session_active,
+			"Session ID" => $session_ID,
+			"Current Session Score" => $latest_session_score
+		];
+
+		return $user;
+	} else {
+		// No user exists with given email address
+		return -1;
+	}
+}
+
+
+/*
+ *	Verifies a username-password combination as existing within
+ *	the Users table.
+ *	
+ *	Inputs:
+ *		- $user_email: a string containing the email of the user
+ *		- $password: a string containing the password of the user
+ *	
+ *	Outputs: the user info array if user-password combo exists, 0 if
+ *	the password is incorrect and -1 if the email is not in the table.
+ */
+function verify_user($user_email, $password){
+	
+	$user = fetch_user_email($user_email);
+	
+	// Checks if email exists in table
+	if(gettype($user) == "array"){
+		
 		// Checks if password is the same as the stored hash
-		if(password_verify($password, $pass_hash)){
+		if(password_verify($password, $user["Password Hash"])){
 			
-			// Creates user info array to return
-			$user = [
-				"ID" => $user_ID,
-				"Email" => $user_email,
-				"Session Active" => $session_active,
-				"Session ID" => $session_ID,
-				"Current Session Score" => $latest_session_score
-			];
+			// Erases password hash from return for frontend response
+			unset($user["Password Hash"]);
 			return $user;
 			
 		} else {
@@ -96,7 +169,7 @@ function verify_user($user_email, $password){
  *	others with to join the session.
  *	
  *	Inputs:
- *		- $user_ID: a string containing the ID of the user
+ *		- $user_ID: an integer containing the ID of the user
  *		- $title: a string for the title of the session
  *		- $length: an integer length of the session's time in minutes 
  *	
@@ -130,7 +203,8 @@ function create_session($user_ID, $title, $length){
  *	Adds a user to a session using the share code given to them
  *	
  *	Inputs:
- *		- $user_ID: a string containing the ID of the user
+ *		- $user_ID: an integer containing the ID of the user
+ 
  *		- $share_code: a string 7 digit alphanumeric invite code
  *	
  *	Outputs: an array of information about the session if successfully
@@ -192,7 +266,6 @@ function join_session($user_ID, $share_code){
 		return -1;
 	}
 }
-//var_dump(join_session(5, "5d86322"));
 
 /*
  *	Sets the start time of the session to UTC time at the moment
@@ -211,14 +284,190 @@ function start_session($session_ID){
 	$stmt->bind_param("ii", $time, $session_ID);
 	$result = $stmt->execute();
 	$stmt->close();
+	return $time;
+}
+
+/*
+ *	Returns the current stored data of all members of a session
+ *	
+ *	Inputs:
+ *		- $session_ID: a unique integer ID for a session
+ *	
+ *	Outputs: an array of data for each member of the session, -1
+ *	if session ID is invalid or SQL fails
+ */
+function running_session($session_ID){
+	global $mysqli;
+	$query = "SELECT `Start Time` FROM `Active Sessions` WHERE ID=?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("i", $session_ID);
+	$stmt->bind_result($start_time);
+	$result = $stmt->execute();
+	
+	// If SQL query was successful
+	if($result){
+		
+		// If session with given ID exists
+		if($stmt->fetch()){
+			$stmt->close();
+			
+			// If on standby, start time is 0
+			if($start_time == 0){
+				return false;
+			} else {
+				return true;
+			}
+		
+		// If session ID does not match any active session
+		} else {
+			return -1;
+		}
+	} else {
+		return -1;
+	}
+}
+
+/*
+ *	Returns data of all members of a session
+ *	
+ *	Inputs:
+ *		- $session_ID: a unique integer ID for a session
+ *	
+ *	Outputs: an array of scores for each member of the session, -1
+ *	if session ID is invalid
+ */
+function about_session($session_ID){
+	global $mysqli;
+	$query = "SELECT Members FROM `Active Sessions` WHERE ID=?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("i", $session_ID);
+	$stmt->bind_result($members);
+	$result = $stmt->execute();
+	$member_data = [];
+	
+	// If SQL query was successful
+	if($result){
+		
+		// If session with given ID exists
+		if($stmt->fetch()){
+			$stmt->close();
+			$members = json_decode($members);
+			foreach($members as $member){
+				array_push($member_data, fetch_user_ID($member));
+			}
+			return $member_data;
+		
+		// If session ID does not match any active session
+		} else {
+			return -1;
+		}
+	} else {
+		return 0;
+	}
+}
+
+/*
+ *	Returns the current scores of all members of a session
+ *	
+ *	Inputs:
+ *		- $session_ID: a unique integer ID for a session
+ *	
+ *	Outputs: an array of scores for each member of the session, -1
+ *	if session ID is invalid
+ */
+function list_session($session_ID){
+	global $mysqli;
+	$query = "SELECT ID, `Latest Session Score` FROM Users WHERE `Session ID`=?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("i", $session_ID);
+	$stmt->bind_result($user_ID, $score);
+	$stmt->execute();
+	$member_scores = [];
+	while($stmt->fetch()){
+		$member_scores[$user_ID] = $score;
+	}
+	$stmt->close();
+	return $member_scores;
+}
+
+/*
+ *	Updates the focus score for a particular user
+ *	
+ *	Inputs:
+ *		- $user_ID: a unique integer ID for a session
+ *		- $score: the float score that the user currently has
+ *	
+ *	Outputs: true if successfully started, false otherwise
+ */
+function update_score($user_ID, $score){
+	global $mysqli;
+	$query = "UPDATE Users SET `Latest Session Score`=? WHERE ID=?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("di", $score, $user_ID);
+	$result = $stmt->execute();
+	$stmt->close();
 	return $result;
 }
-//var_dump(start_session(5));
 
-function status_session($session_ID){
-	
-}
 
+/*
+ *	Closes a session
+ *	
+ *	Inputs:
+ *		- $session_ID: a unique integer ID for a session
+ *	
+ *	Outputs: true if successfully ended, false otherwise
+ */
 function close_session($session_ID){
+	global $mysqli;
 	
+	// Resets all users to being inactive (no running session)
+	$query = "UPDATE Users SET `Session Active`=0 WHERE `Session ID`=?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("i", $session_ID);
+	$result = $stmt->execute();
+	$stmt->close();
+	
+	// If SQL Update statement successful
+	if($result) {
+		
+		// Gets information about session to archive
+		$query = "SELECT Title, Members, Length FROM `Active Sessions` WHERE ID=?";
+		$stmt = $mysqli->prepare($query);
+		$stmt->bind_param("i", $session_ID);
+		$stmt->bind_result($session_title, $members, $length);
+		$stmt->execute();
+		
+		// If SQL Select statement successful
+		if($stmt->fetch()){
+			$stmt->close();
+			
+			// Create archive entry of session
+			$query = "INSERT INTO `Sessions History` (ID, Title, Members, Length) VALUES (?, ?, ?, ?)";
+			$stmt = $mysqli->prepare($query);
+			$stmt->bind_param("issi", $session_ID, $session_title, $members, $length);
+			$result = $stmt->execute();
+			$stmt->close();
+			
+			// If SQL Insert statement successful
+			if($result){
+				
+				// Remove active session, now that archive has been created
+				$query = "DELETE FROM `Active Sessions` WHERE ID=?";
+				$stmt = $mysqli->prepare($query);
+				$stmt->bind_param("i", $session_ID);
+				return $stmt->execute();
+
+			} else {
+				$stmt->close();
+				return false;
+			}
+		} else {
+			return false;
+		}
+		
+	} else {
+		return false;
+	}
 }
+var_dump(close_session(5));
